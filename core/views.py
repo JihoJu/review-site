3c02d7d3d919d9b16b9f83fffab30576d0df4409
ziho <75026409+jiahho@users.noreply.github.com>
2021-07-22 17:32:44 +0900

    Movie, Book, Person 최신 순으로 resolve_home에서 보여줌 & 각 obj_list 마다 Paginator 생성

--- core/views.py
+++ core/views.py
@@ -1,8 +1,36 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from movies.models import Movie
+from books.models import Book
+from people.models import Person
 
 
 def resolve_home(request):
-    return render(request, "home.html")
+    movies_page = request.GET.get("movies_page")
+    books_page = request.GET.get("books_page")
+    people_page = request.GET.get("people_page")
+
+    latest_movies_list = Movie.objects.all().order_by("-created")
+    latest_books_list = Book.objects.all().order_by("-created")
+    latest_people_list = Person.objects.all().order_by("-created")
+
+    paginator_movies = Paginator(latest_movies_list, 5, orphans=3)
+    paginator_books = Paginator(latest_books_list, 5, orphans=3)
+    paginator_people = Paginator(latest_people_list, 5, orphans=3)
+
+    latest_movies = paginator_movies.get_page(movies_page)
+    latest_books = paginator_books.get_page(books_page)
+    latest_people = paginator_people.get_page(people_page)
+
+    return render(
+        request,
+        "home.html",
+        context={
+            "latest_movies": latest_movies,
+            "latest_books": latest_books,
+            "latest_people": latest_people,
+        },
+    )
 
 
 def resolve_search(request):
